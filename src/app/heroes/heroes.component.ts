62aac264f29783b62f139ad2611682ba3f9a9f0f
Peter Dziuba <PeterDziuba@gmail.com>
2018-06-18 14:00:28 -0500

    finished first angular tutorial!

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,26 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   getHeroes(): void {
   this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-  onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-  }
   constructor(private heroService: HeroService) { }
   ngOnInit() {
       this.getHeroes();
+  }
+
+  add(name: string): void {
+      name = name.trim();
+      if (!name) { return; }
+      this.heroService.addHero({ name } as Hero)
+        .subscribe(hero => {
+          this.heroes.push(hero);
+      });
+  }
+  
+  delete(hero: Hero): void {
+      this.heroes = this.heroes.filter(h => h !== hero);
+      this.heroService.deleteHero(hero).subscribe();
   }
 }
